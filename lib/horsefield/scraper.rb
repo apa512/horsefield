4aa793ffb7a59751addafd9ae1afd405558f0b62
Erik Strömberg <erik.stromberg@gmail.com>
2014-06-25 18:53:19 +0200

    Not needed

--- lib/horsefield/scraper.rb
+++ lib/horsefield/scraper.rb
@@ -15,7 +15,7 @@ module Horsefield
     end
 
     def html
-      @html ||= @url_or_html =~ /\A#{URI::regexp}\Z/ ? open(@url_or_html).read : @url_or_html
+      @url_or_html =~ /\A#{URI::regexp}\Z/ ? open(@url_or_html).read : @url_or_html
     end
   end
 end
