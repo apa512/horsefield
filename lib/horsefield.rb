585e61536a6dea22ecedaad28a1dece944750922
Erik Strömberg <erik.stromberg@gmail.com>
2015-02-02 20:45:59 +0100

    Can't require development dependency

--- lib/horsefield.rb
+++ lib/horsefield.rb
@@ -2,7 +2,6 @@ require 'horsefield/version'
 require 'horsefield/scraper'
 require 'horsefield/diggable'
 require 'horsefield/nokogiri'
-require 'pry'
 
 module Horsefield
   # Your code goes here...
