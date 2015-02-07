ed2bfd5554e1e20528b736a4fd221c29cbb3d180
Erik Strömberg <erik.stromberg@gmail.com>
2015-02-07 02:03:13 +0100

    Change tests

--- test/horsefield/test_scraper.rb
+++ test/horsefield/test_scraper.rb
@@ -1,77 +1,28 @@
 require 'test_helper'
 require 'pry'
 
-class RecipeScraper
+class RedditScraper
   include Horsefield::Scraper
 
-  helper :convert_time do |str|
-    str.upcase
+  many :posts, '#siteTable .thing' do
+    one :title, 'a.title'
+    one :tagline, 'p.tagline' do
+      one :submitted, './time/@datetime'
+      one :subreddit, 'a.subreddit'
+    end
+    many :links, './/a[contains(@href, "reddit.com")]/@href'
   end
 
-  scope '.hide_from_screen' do
-    one :title, 'h1'
-    one :description, 'h2'
-  end
-
-  many :nutritional_value, 'article.recipe_nutrition ul li' do
-    one(:type) { at('span').text }
-    one(:value) { at('span.value').text }
-  end
-
-  one :total_time, '//div[contains(@style, "time.gif")]' do
-    convert_time(text.strip)
-  end
-
-  postprocess do |doc|
-    doc[:nutritional_value] = doc[:nutritional_value].uniq.compact
-    doc
-  end
-end
-
-class RecipeScraperWithOptionalField
-  include Horsefield::Scraper
-
-  scope '.hide_from_screen' do
-    one :title, 'h1'
-    one :description, 'h2'
-    one? :missing, '.missing'
-  end
-end
-
-class RecipeScraperWithRequiredField
-  include Horsefield::Scraper
-
-  scope '.hide_from_screen' do
-    one :title, 'h1'
-    one :description, 'h2'
-    one! :important, '.important'
-  end
+  many :trending, '.trending-subreddits-content > ul > li a'
 end
 
 class TestScraper < Minitest::Test
   def setup
-    @html = File.read(File.expand_path('../../recipe_source.html', __FILE__)).force_encoding('UTF-8')
+    @html = File.read(File.expand_path('../../reddit_source.html', __FILE__)).force_encoding('UTF-8')
   end
 
-  def test_that_it_scrapes
-    recipe = RecipeScraper.new(@html).scrape
-    assert_equal 'Traditional Welsh cawl', recipe[:title]
-  end
-
-  def test_that_it_ignores_optional_fields
-    recipe = RecipeScraperWithOptionalField.new(@html).scrape
-    refute_includes recipe.keys, :missing
-  end
-
-  def test_that_it_raises_with_missing_required_field
-    assert_raises(Horsefield::MissingSelectorError) { RecipeScraperWithRequiredField.new(@html).scrape }
-  end
-
-  def test_standalone_scraper
-    recipe = Horsefield::Scraper.scrape(@html) do
-      one :title, '.hide_from_screen h1'
-    end
-
-    assert_equal 'Traditional Welsh cawl', recipe[:title]
+  def test_scraper
+    reddit = RedditScraper.new(@html).scrape
+    p reddit
   end
 end
